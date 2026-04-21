44aefd8de61ae3deb8b1ea722c99a0c210bb9348
Italo1994 <italohortiz2015@gmail.com>
2026-04-20 21:57:45 -0300

    feat: create health-records module with vaccines

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { PetsModule } from './pets/pets.module';
+import { dataSourceOptions } from './database/data-source';
 import { PetIdentityModule } from './pet-identity/pet-identity.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { PetsModule } from './pets/pets.module';
+import { UsersModule } from './users/users.module';
+import { HealthRecordsModule } from './health-records/health-records.module';
 
 @Module({
   imports: [
@@ -13,17 +16,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     UsersModule,
     PetsModule,
     TypeOrmModule.forRoot({
-      type: 'postgres', // ou mysql
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'petid',
-
-      autoLoadEntities: true, // 🔥 MUITO IMPORTANTE
-      synchronize: true, // só em dev
+      ...dataSourceOptions,
+      autoLoadEntities: true,
     }),
     PetIdentityModule,
+    HealthRecordsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
